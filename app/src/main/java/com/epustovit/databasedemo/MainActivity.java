b26302b15bd2ff4e540aaca6bff1c7d6b8b0c15d
shpp-epustovit <smithpev@gmail.com>
2017-09-05 20:15:02 +0300

    Advanced SQLite

--- app/src/main/java/com/epustovit/databasedemo/MainActivity.java
+++ app/src/main/java/com/epustovit/databasedemo/MainActivity.java
@@ -17,61 +17,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-//        try {
-//
-//            // create Database
-//            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE,null);
-//
-//            // create a table for Database
-//            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users(name VARCHAR, age INT(3))");
-//
-//            // insert some date into the table
-//            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Yevhenii', 31)");
-//
-//            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Irina', 26)");
-//
-//            // get data out the database
-//            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
-//
-//            int nameIndex = c.getColumnIndex("name");
-//            int ageIndex = c.getColumnIndex("age");
-//
-//            c.moveToFirst();
-//            while (c != null){
-//                Log.i("name", c.getString(nameIndex));
-//                Log.i("age", Integer.toString(c.getInt(ageIndex)));
-//
-//                c.moveToNext();
-//            }
-//
-//        }
-//        catch (Exception e){
-//
-//            e.printStackTrace();
-//        }
-
         try {
-            SQLiteDatabase historicalEvents = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
-            historicalEvents.execSQL("CREATE TABLE IF NOT EXISTS events (event VARCHAR, date INT(4))");
-            historicalEvents.execSQL("INSERT INTO events (event, date) VALUES ('My birthday', 1986)");
-            historicalEvents.execSQL("INSERT INTO events (event, date) VALUES ('Year when I go to school', 1993)");
 
-            Cursor c = historicalEvents.rawQuery("SELECT * FROM events", null);
+            // create Database
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE,null);
 
-            int eventIndex = c.getColumnIndex("event");
-            int dateIndex = c.getColumnIndex("date");
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS newUsers (name VARCHAR, age INTEGER(3), id INTEGER PRIMARY KEY)");
+
+            // create a table for Database
+           // myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users(name VARCHAR, age INT(3))");
+
+            // insert some date into the table
+           // myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Yevhenii', 31)");
+           // myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Irina', 26)");
+           // myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Nastya', 27)");
+//            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Roman', 27)");
+
+            // deleting from database
+            myDatabase.execSQL("DELETE FROM newUsers WHERE id = 4");
+
+            // change in database
+            //myDatabase.execSQL("UPDATE users SET age = 20 WHERE name = 'Irina'");
+
+            // get data out the database
+            Cursor c = myDatabase.rawQuery("SELECT * FROM newUsers", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+            int idIndex = c.getColumnIndex("id");
 
             c.moveToFirst();
-
             while (c != null){
-                Log.i("event", c.getString(eventIndex));
-                Log.i("date", Integer.toString (c.getInt(dateIndex)));
+                Log.i("UsersResult - name", c.getString(nameIndex));
+                Log.i("UsersResult - age", Integer.toString(c.getInt(ageIndex)));
+                Log.i("UsersResult - id", Integer.toString(c.getInt(idIndex)));
 
                 c.moveToNext();
             }
+
         }
         catch (Exception e){
+
             e.printStackTrace();
         }
+
+//        try {
+//            SQLiteDatabase historicalEvents = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
+//            historicalEvents.execSQL("CREATE TABLE IF NOT EXISTS events (event VARCHAR, date INT(4))");
+//            historicalEvents.execSQL("INSERT INTO events (event, date) VALUES ('My birthday', 1986)");
+//            historicalEvents.execSQL("INSERT INTO events (event, date) VALUES ('Year when I go to school', 1993)");
+//
+//            Cursor c = historicalEvents.rawQuery("SELECT * FROM events", null);
+//
+//            int eventIndex = c.getColumnIndex("event");
+//            int dateIndex = c.getColumnIndex("date");
+//
+//            c.moveToFirst();
+//
+//            while (c != null){
+//                Log.i("event", c.getString(eventIndex));
+//                Log.i("date", Integer.toString (c.getInt(dateIndex)));
+//
+//                c.moveToNext();
+//            }
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
     }
 }
